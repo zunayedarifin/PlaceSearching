79a05e46c73a828ba66ae59ced376ac9bf993252
Zunayed <zunayed.arifin@gmail.com>
2021-08-30 21:10:55 +0600

    edittext place search done

--- app/src/main/java/com/arifin/placesearhing/view/MainActivity.kt
+++ app/src/main/java/com/arifin/placesearhing/view/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.arifin.placesearhing.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.arifin.placesearhing.BuildConfig
 import com.arifin.placesearhing.R
 import com.arifin.placesearhing.databinding.ActivityMainBinding
 import com.arifin.placesearhing.model.nearbyplaces.Result
 import com.arifin.placesearhing.viewModel.NearByPlacesViewModel
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +36,15 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        Places.initialize(applicationContext,apiKey)
+        binding.etAddress.setOnClickListener {
+            val fieldList = listOf(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME)
+            Autocomplete.IntentBuilder(OVERLAY, fieldList)
+                .build(this@MainActivity).apply {
+                    this@MainActivity.startActivityForResult(this, 100)
+            }
+
+        }
 
         viewModel.getNearByPlaces(location,radius,types,name,apiKey).observe(this) { response ->
             if (response.status == "OK") {
@@ -45,6 +62,18 @@ class MainActivity : AppCompatActivity() {
             isUpdated=it
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            val place = Autocomplete.getPlaceFromIntent(data!!)
+            binding.etAddress.setText(place.latLng.toString())
+
+        } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+            Autocomplete.getStatusFromIntent(data!!)
+            Toast.makeText(applicationContext,"Something went wrong ",Toast.LENGTH_SHORT).show()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun setValues(list: List<Result>) {
